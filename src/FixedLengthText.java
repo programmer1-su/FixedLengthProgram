ac119585be5d6db3c5fdd520dc4cda78dd9b5cec
programmer1-su <winecherry11@gmail.com>
2020-10-11 20:27:18 +0900

    固定長読み込み処理する

--- src/FixedLengthText.java
+++ src/FixedLengthText.java
@@ -1,26 +1,34 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FixedLengthText {
 
 	public static void main(String[] args) {
-		
 		try {
+			FileInputStream fis = new FileInputStream("/Users/sucherrywine/Documents/UnionTec/file_layout.txt");
+			Scanner sc = new Scanner(fis);
+			String[] nums = {};
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				nums = line.split(",");
+			}
+			sc.close();
 			File file = new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長.txt");
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 			StringBuffer sb = new StringBuffer();
 			String line;
-			int[] nums = { 5,30,1,8,8,10,11,15,15,12 };
-			while((line = br.readLine())!= null) {
+			while ((line = br.readLine()) != null) {
 				int beginIndex = 0;
-				for(int j = 0; j < nums.length; j++) {
-					int endIndex = beginIndex + nums[j];
+				for (int i = 0; i < nums.length; i++) {
+					int endIndex = beginIndex + Integer.parseInt(nums[i]);
 					sb.append(line.substring(beginIndex, endIndex));
-					if(nums.length > j + 1) {
+					if (nums.length > i + 1) {
 						sb.append(",");
 					}
 					beginIndex = endIndex;
@@ -28,14 +36,13 @@ public class FixedLengthText {
 				sb.append("\n");
 			}
 			fr.close();
-			System.out.println(sb.toString());
-			
-			PrintWriter writer = new PrintWriter(new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長_Csv.txt")); 
+			PrintWriter writer = new PrintWriter(new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長_CSV.txt"));
 			writer.write(sb.toString());
 			writer.flush();
 			writer.close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 }
