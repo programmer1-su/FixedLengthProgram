467833091cdcdffa74471474fb73e552b7c12b6a
programmer1-su <winecherry11@gmail.com>
2020-10-18 21:07:15 +0900

    資格取得リスト名ファイル作成する

---
+++ src/CertificateListName.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CertificateListName {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("/Users/sucherrywine/Documents/UnionTec/employee.txt");
+			FileReader fr = new FileReader(file);
+			HashMap<String, String> empMap = new HashMap<String, String>();
+			StringBuilder sb = new StringBuilder();
+			int employee;
+			while ((employee = fr.read()) != -1) {
+				sb.append((char) employee);
+			}
+			fr.close();
+
+			String[] strArray = sb.toString().split("\n");
+			for (int i = 0; i < strArray.length; i++) {
+				if (i == 0) {
+					continue;
+				}
+
+				String[] emp = strArray[i].split(",");
+				empMap.put(emp[0], emp[1]);
+			}
+
+			File listFile = new File("/Users/sucherrywine/Documents/UnionTec/oracle_bronze_certificate_list.txt");
+			FileReader r = new FileReader(listFile);
+			HashMap<String, String> certificateMap = new HashMap<String, String>();
+			StringBuilder sbCertificate = new StringBuilder();
+			int oracleCertificate;
+			while ((oracleCertificate = r.read()) != -1) {
+				sbCertificate.append((char) oracleCertificate);
+			}
+			r.close();
+
+			String[] certificateArr = sbCertificate.toString().split("\n");
+			for (int j = 0; j < certificateArr.length; j++) {
+				if (j == 0) {
+					continue;
+				}
+				String[] certificate = certificateArr[j].split(",");
+				certificateMap.put(certificate[1], certificate[0]);
+			}
+
+			StringBuilder sbFinal = new StringBuilder();
+			sbFinal.append("資格取得日,社員番号,社員名\n");
+
+			for (Map.Entry<String, String> certificateEntry : certificateMap.entrySet()) {
+				String code = certificateEntry.getKey();
+				String date = certificateEntry.getValue();
+				String name = empMap.get(code.trim());
+				sbFinal.append(date.trim() + "," + code.trim() + "," + name.trim() + "\n");
+			}
+
+			PrintWriter pw = new PrintWriter(
+					new File("/Users/sucherrywine/Documents/UnionTec/oracle_bronze_certificate_list_name.txt"));
+			pw.write(sbFinal.toString());
+			pw.close();
+			pw.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
--- src/FixedLengthText.java
+++ src/FixedLengthText.java
@@ -10,17 +10,21 @@ public class FixedLengthText {
 
 	public static void main(String[] args) {
 		try {
-			FileInputStream fis = new FileInputStream("/Users/sucherrywine/Documents/UnionTec/file_layout.txt");
-			Scanner sc = new Scanner(fis);
+			FileInputStream fis = new FileInputStream("/Users/sucherrywine/Documents/UnionTec/file_layout.txt");//rawバイトのストリームを読み込むときは、FileInputStreamを使用してください。
+			//FileInputStream is useful to read data from a file in the form of sequence of bytes. 
+			//FileInputStream is meant for reading streams of raw bytes such as image data.
+			Scanner sc = new Scanner(fis);//to read text files
 			String[] nums = {};
+			// iterate till end 
 			while (sc.hasNextLine()) {
 				String line = sc.nextLine();
 				nums = line.split(",");
 			}
 			sc.close();
-			File file = new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長.txt");
-			FileReader fr = new FileReader(file);
+			File file = new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長.txt");//specify the filename
+			FileReader fr = new FileReader(file);//FileReaderは、文字のストリームを読み込むために使用されます。
 			BufferedReader br = new BufferedReader(fr);
+			// StringBufferを宣言する
 			StringBuffer sb = new StringBuffer();
 			String line;
 			while ((line = br.readLine()) != null) {
@@ -29,20 +33,48 @@ public class FixedLengthText {
 					int endIndex = beginIndex + Integer.parseInt(nums[i]);
 					sb.append(line.substring(beginIndex, endIndex));
 					if (nums.length > i + 1) {
-						sb.append(",");
+						sb.append(",");// appendメソッドを使用して、commaを結合する
 					}
 					beginIndex = endIndex;
 				}
 				sb.append("\n");
 			}
 			fr.close();
+			//Javaにはファイルの書き込みを行うFileWriterクラスがあります。FileWriterクラスを使って新しいテキストファイルを作ったり、すでに存在するテキストファイルに追加で文字を書き込むことができます。
 			PrintWriter writer = new PrintWriter(new File("/Users/sucherrywine/Documents/UnionTec/労働者名簿_固定長_CSV.txt"));
+			//行の自動フラッシュは行わずに、指定されたファイルで新しいPrintWriterを作成します。
 			writer.write(sb.toString());
-			writer.flush();
-			writer.close();
+			writer.flush();// flush the writer
+			writer.close();// flush the stream again
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 }
+//BufferedReaderとは
+//BufferedReaderクラスとは、テキストファイルを読み込むためのクラスです。
+
+//BufferedReaderクラスでは、テキストファイルを1行ずつ読み込むreadlineメソッドが用意されています。
+
+//テキストファイルを読み込むクラスにはFileReaderクラスもありますが、こちらはテキストを1文字ずつ読み込むため効率が悪い場合があります。
+
+//そのため、テキストファイルの読み込みはBufferedReaderクラスを使うようにしましょう。
+
+//BufferedReaderの使い方
+//ここでは、BufferedReaderクラスのreadLineメソッドを使って、テキストファイルを1行ずつ読み込む方法を解説します。
+
+//readLineメソッドは指定したテキストファイルを1行ずつ読み込み、String型の戻り値として返します。
+
+//ファイルの終わりに到達した場合は「null」を返します。
+
+//そのため、ファイルの終わりまで読み込む場合には、while文でnullが返されるまでループさせます。
+
+//テキストファイルを1行ずつ読み込む方法について、次のプログラムで確認してみましょう。
+
+//StringBufferとは
+//StringBufferクラスは、Stringクラス同様に宣言した変数に文字列を格納するために使用します。
+
+//Stringクラスとの違いとして、変数に文字列を格納したあとでも「値を追加」「挿入」「変更」などの、文字列操作が可能なことが挙げられます。
+
+//つまり、StringBufferクラスは、文字列の値が「不変である」と分かっているときに、使用するケースが多いと言えます。
